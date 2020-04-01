cff0266c5c927c3d6e59b5fff868c97219792f08
javipelopi <javipelopincho@hotmail.com>
2020-04-01 19:49:31 +0200

    Augmented simplicity for the user
    Passing a map is far from ideal as many typing errors can occur.
    It would be better to have all the options as standalone values.

--- lib/core_card_io.dart
+++ lib/core_card_io.dart
@@ -3,10 +3,38 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class CoreCardIo {
-  static const MethodChannel _channel =
-      const MethodChannel('core_card_io');
+  static const MethodChannel _channel = const MethodChannel('core_card_io');
 
-  static Future<dynamic> scanCard(Map<String, dynamic> args) {
-    return _channel.invokeMethod('scanCard', args);
+  static Future<dynamic> scanCard({
+    bool requireExpiry = false,
+    bool requireCVV = false,
+    bool requirePostalCode = false,
+    bool requireCardHolderName = false,
+    bool restrictPostalCodeToNumericOnly = false,
+    bool scanExpiry = true,
+    String scanInstructions,
+    bool suppressManualEntry = false,
+    bool suppressConfirmation = false,
+    bool useCardIOLogo = false,
+    bool hideCardIOLogo = false,
+    bool usePayPalActionbarIcon = true,
+    bool keepApplicationTheme = false,
+  }) {
+    var arguments = {
+      "requireExpiry": requireExpiry,
+      "requireCVV": requireCVV,
+      "requirePostalCode": requirePostalCode,
+      "requireCardHolderName": requireCardHolderName,
+      "restrictPostalCodeToNumericOnly": restrictPostalCodeToNumericOnly,
+      "scanExpiry": scanExpiry,
+      "scanInstructions": scanInstructions,
+      "suppressManualEntry": suppressManualEntry,
+      "suppressConfirmation": suppressConfirmation,
+      "useCardIOLogo": useCardIOLogo,
+      "hideCardIOLogo": hideCardIOLogo,
+      "usePayPalActionbarIcon": usePayPalActionbarIcon,
+      "keepApplicationTheme": keepApplicationTheme,
+    };
+    return _channel.invokeMethod('scanCard', arguments);
   }
 }
